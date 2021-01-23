ab99cf204429fa7fa76af4c6a5681e18449a7113
Robert Moore <moore.robert.ryan@gmail.com>
2021-01-23 14:05:33 +0200

    initial  commit

---
+++ background.js
@@ -0,0 +1,35 @@
+var contextMenuItem = {
+    "id": "invertCase",
+    "title": "Invert",
+    "contexts": ["selection"]
+};
+
+function invertText(selectedText){
+
+    var returnText = "";
+
+    for(var i=0; i < selectedText.length; i++){
+        if(/[a-z]/.test(selectedText.charAt(i))){
+            returnText = returnText + selectedText.charAt(i).toUpperCase();
+        }
+        if(/[A-Z]/.test(selectedText.charAt(i))){
+             returnText = returnText + selectedText.charAt(i).toLowerCase();
+         }
+         if(/[^a-zA-Z]/.test(selectedText.charAt(i))){
+            returnText = returnText + selectedText.charAt(i);
+        }
+    }
+    return returnText;
+}
+
+chrome.contextMenus.create(contextMenuItem);
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+   if(clickData.menuItemId === "invertCase" && clickData.selectionText){
+        var selectedText = clickData.selectionText  //program crashes if we access clickData.selectionText without putting it in a var
+        console.log(selectedText);
+        var replacementText = invertText(selectedText);
+        console.log(replacementText);
+
+    }});
+    
