aafff67570cee4cdbb3d7db18287dad92f1e0dfe
Robert Moore <moore.robert.ryan@gmail.com>
2021-01-24 12:56:00 +0200

    prev commit seems to have worked during testing but broke shortly after. Reverting for now. Will investigate in future

--- background.js
+++ background.js
@@ -1,42 +1,39 @@
 var contextMenuItem = {
-  id: "invertCase",
-  title: "Invert",
-  contexts: ["selection"],
+    "id": "invertCase",
+    "title": "Invert",
+    "contexts": ["selection"]
 };
 
-function invertText(selectedText) {
-  var returnText = "";
+function invertText(selectedText){
 
-  for (var i = 0; i < selectedText.length; i++) {
-    if (/[a-z]/.test(selectedText.charAt(i))) {
-      returnText = returnText + selectedText.charAt(i).toUpperCase();
+    var returnText = "";
+
+    for(var i=0; i < selectedText.length; i++){
+        if(/[a-z]/.test(selectedText.charAt(i))){
+            returnText = returnText + selectedText.charAt(i).toUpperCase();
+        }
+        if(/[A-Z]/.test(selectedText.charAt(i))){
+             returnText = returnText + selectedText.charAt(i).toLowerCase();
+         }
+         if(/[^a-zA-Z]/.test(selectedText.charAt(i))){
+            returnText = returnText + selectedText.charAt(i);
+        }
     }
-    if (/[A-Z]/.test(selectedText.charAt(i))) {
-      returnText = returnText + selectedText.charAt(i).toLowerCase();
-    }
-    if (/[^a-zA-Z]/.test(selectedText.charAt(i))) {
-      returnText = returnText + selectedText.charAt(i);
-    }
-  }
-  return returnText;
+    return returnText;
 }
 
 chrome.contextMenus.create(contextMenuItem);
 
-chrome.contextMenus.onClicked.addListener(function (clickData) {
-  //only create the context menu item if field is editable and the there's selected text
-  if (document.activeElement.isContentEditable && clickData.selectionText) {
-    chrome.contextMenus.create(contextMenuItem);
+chrome.contextMenus.onClicked.addListener(function(clickData){
+   if(clickData.menuItemId === "invertCase" && clickData.selectionText){
+        var selectedText = clickData.selectionText  //program crashes if we access clickData.selectionText without putting it in a var
+        console.log(selectedText);
+        var replacementText = invertText(selectedText);
+        console.log(replacementText);
 
-    if (clickData.menuItemId === "invertCase" && clickData.selectionText) {
-      var selectedText = clickData.selectionText; //program crashes if we access clickData.selectionText without putting it in a var
-      console.log(selectedText);
-      var replacementText = invertText(selectedText);
-      console.log(replacementText);
+        chrome.storage.local.set({'invertedText': replacementText});
 
-      chrome.storage.local.set({ invertedText: replacementText });
-
-      chrome.tabs.executeScript({ file: "content.js" });
-    }
-  }
-});
+        chrome.tabs.executeScript({file: 'content.js'});
+    
+    }});
+    
